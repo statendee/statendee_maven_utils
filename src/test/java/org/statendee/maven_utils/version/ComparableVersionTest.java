a1d0f477e86b0cb094dde4c267beebd9640f46f8
jojomatik <dev@jojomatik.de>
2021-12-12 21:44:12 +0100

    refactor: move unit tests into correct package
    Moves the unit tests for `ComparableVersion.java` into the corresponding package.

--- src/test/java/org/statendee/maven_utils/version/ComparableVersionTest.java
+++ src/test/java/org/statendee/maven_utils/version/ComparableVersionTest.java
@@ -13,12 +13,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
  */
-package org.statendee.maven_utils;
+package org.statendee.maven_utils.version;
 
 import org.junit.jupiter.api.Test;
-import org.statendee.maven_utils.version.ComparableVersion;
-import org.statendee.maven_utils.version.NoSnapshotVersionException;
-import org.statendee.maven_utils.version.NoTimestampException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
